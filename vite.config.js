2f265a8da814f51fb185f8df29f3a94f52c878eb
Jonathan Lee <jonjlee@gmail.com>
2025-04-02 17:24:06 -0700

    Move app back to /

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/resphone/',
+    base: '/',
     root: '.',
     server: {
         port: 3000,
